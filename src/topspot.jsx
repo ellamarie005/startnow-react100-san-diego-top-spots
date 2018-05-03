43cfcbaf4a0454cc2ef5af34249c8b2e9ab16f8e
ellaT <ellatolentino05@gmail.com>
2018-05-02 19:10:48 -0700

    update card gaps

--- src/topspot.jsx
+++ src/topspot.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default props => (
-    <div className='card'>
+    <div className='card mb-4'>
         <h4>{props.name}</h4>
         <p>{props.description}</p>
         <a href={`https://maps.google.com/?q=${props.location}`}>Google Map</a>
